7dbf5cb372082c5795393974199b82a7094c9c40
bendav10000-hub <bendav10000@gmail.com>
2026-01-14 20:07:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Day 1', 'Day 2', 'Day 3'],
-    datasets: [{
-      label: 'Test Price',
-      data: [400, 420, 410]
-    }]
-  }
-});
 
 
